4f043daee38353c5b392e7aa427339e6a61e8e8b
Felipe Barcelos <felipebarcelospro@Felipes-iMac-Pro.local>
2025-02-26 10:08:41 -0300

    chore: update post build script

--- tsup.config.ts
+++ tsup.config.ts
@@ -29,24 +29,24 @@ export default defineConfig([
   // Configuração específica para o código client-side
   {
     entry: {
+      'index': 'src/client/index.ts',
       'igniter.context': 'src/client/igniter.context.tsx',
       'igniter.hooks': 'src/client/igniter.hooks.ts',
       'igniter.client': 'src/client/igniter.client.ts',
     },
     format: ['cjs', 'esm'],
-    dts: {
-      entry: 'src/client/index.ts',
-    },
-    splitting: false,
-    sourcemap: true,
+    dts: true,
+    splitting: true, // Habilita o code splitting
     treeshake: true,
     outDir: 'dist/client',
     external: ['react', 'react-dom'],
-    onSuccess: "node scripts/post-build.js", // Adiciona a diretiva 'use client' nos arquivos de saída
     esbuildOptions(options) {
       options.banner = {
         js: "'use client';",
       };
+      options.bundle = true;
+      options.platform = 'neutral';
+      options.mainFields = ['module', 'main'];
     },
   }
 ])
